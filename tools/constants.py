1e15787d0a481ca9d625c6154a7c967c8ac5f586
Jorick Lania <j.f.lania@student.rug.nl>
2024-10-28 15:43:55 +0100

    Set up repository structure. Grouped original CETO source code into appropriate directory. Created constants.py file. Created initial conditions files in dictionary formats.

---
+++ tools/constants.py
@@ -0,0 +1,59 @@
+#### Physical, numerical and chemical constants
+
+#### Astronomical Constants
+M_earth = 5.972e24          #kg
+R_earth = 6.335439e6        #m
+M_core_earth = 1.94e24      #kg
+R_core_earth = 3485000      #m
+
+#### Physical Constants
+const_G = 6.67430e-11       # Gravitational Constant, N m^2 / kg^2 (CODATA 2024)
+mol = 6.02214076e+23
+R_gas = 8.31446261815324    #gas constant, J/K/mol
+k_b   = 1.3806485279e-23    #boltzman constant, J/K
+
+#### Chemical Constants
+## Molecular Weights (g/mol)
+molwts = {
+    "H"       : 1.00794,
+    "MgO"     : 40.3044,
+    "SiO"     : 60.08,
+    "FeO"     : 71.844,
+    "MgSiO3"  : 100.39,
+    "FeSiO3"  : 131.9287,
+    "Na2O"    : 61.9789,
+    "Na2SiO3" : 122.063,
+    "H2"      : 2.016,
+    "H2O"     : 18.01528,
+    "CO"      : 28.01,
+    "CO2"     : 44.0095,
+    "Fe"      : 55.847,
+    "Si"      : 28.0855,
+    "O"       : 15.9994,
+    "CH4"     : 16.04,
+    "O2"      : 31.9988,
+    "Mg"      : 24.305,
+    "SiO"     : 60.08,
+    "Na"      : 22.98977,
+    "SiH4"    : 32.12
+}
+
+## Approximate Molar Volumes (cm^3 / mol)
+V_MgO = 1.246
+V_SiO2 = 2.71/2
+V_MgSiO3 = 3.958
+V_FeO = 1.319
+V_FeSiO3 = 4.031
+V_Na2O = 2.997
+V_Na2SiO3 = 5.709
+V_H2 = 1.10
+V_H2O = 1.92
+V_CO = 3.958
+V_CO2 = 2.3
+V_Fe = 0.776
+V_Si = 1.0886
+V_O = 0.4
+V_H = 0.266
+
+#### Other Constants
+log_to_ln = 2.302585093     #conversion factor
